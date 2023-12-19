8e0c33516c2b2c80a83ee1b66131f74bd1a9953b
JPotti <129278920+JPotti@users.noreply.github.com>
2023-12-19 23:01:16 +0100

    Add files via upload

---
+++ Code for removing duplicate lines.py
@@ -0,0 +1,23 @@
+def remove_duplicate_lines(file_path):
+    lines_seen = set()
+    output_lines = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Remove leading and trailing whitespaces and then add to set
+            cleaned_line = line.strip()
+            if cleaned_line not in lines_seen:
+                output_lines.append(line)
+                lines_seen.add(cleaned_line)
+
+    with open(file_path, 'w') as file:
+        for line in output_lines:
+            file.write(line)
+
+# Specify the path to your text file
+file_path = 'path_to_text_file'
+
+# Call the function to remove duplicates
+remove_duplicate_lines(file_path)
+
+print('Duplicates removed successfully.')
